05a8905c5ff1777ccb7b467b21d1c731f328ebe6
Bayaman Gapparov <bayamangapparov@gmail.com>
2020-03-22 12:09:50 -0500

    Parsing

--- src/main/java/com/bgapparov/coronavirustraccker/CoronavirusTracckerApplication.java
+++ src/main/java/com/bgapparov/coronavirustraccker/CoronavirusTracckerApplication.java
@@ -2,8 +2,10 @@ package com.bgapparov.coronavirustraccker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CoronavirusTracckerApplication {
 
 	public static void main(String[] args) {
